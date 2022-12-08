a5af8cbda705605eda4cd4d095431b1ce8565443
krishdd2 <13781821+krishdd2@users.noreply.github.com>
2022-12-08 01:34:35 -0500

    Update and rename west-zone.tf to domain-1.tf

--- modules/zones/domain-1.tf
+++ modules/zones/domain-1.tf
@@ -1,10 +1,10 @@
 # Tp create public hosted zone and subdomain zone for that 
 resource "aws_route53_zone" "kk-main-domain" {
-  name = "kk-shop-us.com"
+  name = "kk-shop-us-1.com"
 }
 
 resource "aws_route53_zone" "west-sub-domain" {
-  name = "west.kk-shop-us.com"
+  name = "west.kk-shop-us-1.com"
 
   tags = {
     Environment = "dev"
