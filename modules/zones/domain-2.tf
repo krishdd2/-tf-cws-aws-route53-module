04285c017d2bf6e0d14f32df9f3ad99c979ff647
krishdd2 <13781821+krishdd2@users.noreply.github.com>
2022-12-08 17:11:02 -0500

    Update domain-2.tf

--- modules/zones/domain-2.tf
+++ modules/zones/domain-2.tf
@@ -1,9 +1,9 @@
-# Tp create public hosted zone and subdomain zone for that 
+# Tp create public hosted zone and subdomain zone for that
 resource "aws_route53_zone" "kk-main-domain-2" {
   name = "kk-shop-2-us.com"
 }
 
-resource "aws_route53_zone" "west-sub-domain" {
+resource "aws_route53_zone" "east-sub-domain" {
   name = "east.kk-shop-2-us.com"
 
   tags = {
